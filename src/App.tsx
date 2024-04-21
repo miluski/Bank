257f60ec9516ec3f09445c7b55caca49f285d8d7
sprytnykowal@gmail.com <sprytnykowal@gmail.com>
2024-04-21 20:43:09 +0200

    Implemented create deposit view

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 import SendingTransferView from "./views/SendingTransferView/SendingTransferView";
+import CreateDepositView from "./views/CreatingDepositView/CreateDepositView";
 
 function App() {
 	const persistConfig = {
@@ -30,7 +31,7 @@ function App() {
 						<Route path='/open-account' element={<OpenAccountView />} />
 						<Route path='/dashboard' element={<Dashboard />} />
 						<Route path='/send-transfer' element={<SendingTransferView />} />
-						<Route path='/create-deposit' element={<></>} />
+						<Route path='/create-deposit' element={<CreateDepositView />} />
 					</Routes>
 				</BrowserRouter>
 			</PersistGate>
